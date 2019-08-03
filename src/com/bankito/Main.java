ced5165b868749d2cbaa74c71cf3a00d635e7134
Enrique <quicolo@gmail.com>
2019-08-03 07:56:13 +0200

    Primera versión de la pantalla de login y del esqueleto de la pantalla principal del cliente

--- src/com/bankito/Main.java
+++ src/com/bankito/Main.java
@@ -5,20 +5,99 @@
  */
 package com.bankito;
 
+import com.bankito.aplicacion.ClienteViewController;
+import com.bankito.aplicacion.LoginViewController;
 import com.bankito.aplicacion.MainCont;
+import com.bankito.servicio.ServicioBancario;
+import com.bankito.servicio.ServicioBancarioFactory;
 import com.bankito.util.AppConfiguration;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.fxml.JavaFXBuilderFactory;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 /**
  *
  * @author Kike
  */
-public class Main {
-    public static void main (String args[]) throws IOException{
-        AppConfiguration.loadProperties();
-        
-        MainCont mainCont = new MainCont();
-        mainCont.accionPrincipal();
+public class Main extends Application {
+    private Stage stage;
+    private ServicioBancario servicioBancario;
+
+    public static void main(String[] args) {
+        // Lanza la aplicación JavaFX llamando al método estático heredado launch.
+        // launch crea una instancia de Application, llama al método init(), crea el
+        // escenario principal (primaryStage) y llama al método start entre otras cosas
+        Application.launch(args);
+
     }
     
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        AppConfiguration.loadProperties();
+        this.stage = primaryStage;
+        servicioBancario = ServicioBancarioFactory.create();
+        
+        primaryStage.getIcons().add(new Image(Main.class.getResourceAsStream("presentacion/images/Bankito Logo app.png")));
+        primaryStage.setTitle("Bankito");
+        goToLogin();
+        primaryStage.show();
+    }
+
+    
+    
+    public void goToLogin() {
+        try {
+            LoginViewController login = (LoginViewController) replaceSceneContent("presentacion/LoginView.fxml");
+            login.setMainApp(this);
+        } catch (Exception ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    
+    }
+    
+    public void goToClienteView(){
+        try {
+            ClienteViewController login = (ClienteViewController) replaceSceneContent("presentacion/ClienteView.fxml");
+            login.setMainApp(this);
+        } catch (Exception ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    private Initializable replaceSceneContent(String fxml) throws Exception {
+        FXMLLoader loader = new FXMLLoader();
+        InputStream in = Main.class.getResourceAsStream(fxml);
+        loader.setBuilderFactory(new JavaFXBuilderFactory());
+        loader.setLocation(Main.class.getResource(fxml));
+        AnchorPane page;
+        try {
+            page = (AnchorPane) loader.load(in);
+        } finally {
+            in.close();
+        } 
+        Scene scene = new Scene(page);
+        stage.setScene(scene);
+        stage.sizeToScene();
+        return (Initializable) loader.getController();
+    }
+    
+    public ServicioBancario getServicioBancario() {
+        return servicioBancario;
+    }
+
+    public void setServicioBancario(ServicioBancario servicio) {
+        this.servicioBancario = servicio;
+    }
+
 }
---
+++ src/com/bankito/aplicacion/ClienteViewController.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bankito.aplicacion;
+
+import com.bankito.Main;
+import com.bankito.dominio.exceptions.DominioException;
+import com.bankito.servicio.ServicioBancario;
+import com.bankito.servicio.dto.UsuarioDto;
+import com.bankito.servicio.exceptions.ServicioException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author Kike
+ */
+public class ClienteViewController implements Initializable {
+    
+    private Main mainApp;
+
+    @FXML
+    private Button cerrarSesionBtn;
+
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+    }    
+    
+    @FXML
+    public void accionLogout(ActionEvent event) {
+        try {
+            ServicioBancario sb = mainApp.getServicioBancario();
+            
+            boolean result = sb.logoutUsuario();
+            
+            if(result == true)           
+                mainApp.goToLogin();
+            else
+                System.out.println("Error raro");
+        } catch (DominioException ex) {
+            Logger.getLogger(LoginViewController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ServicioException ex) {
+            Logger.getLogger(LoginViewController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+    
+    public void setMainApp(Main aplicacion) {
+        this.mainApp = aplicacion;
+    }
+}
